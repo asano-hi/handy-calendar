d53713665edb9912d2878aae15120a0cb9ce0bc9
asano <asanohiroto@asano-Mac.local>
2020-09-26 16:47:55 +0900

    timeformat change

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,12 +12,10 @@ class JobsController < ApplicationController
     end
 
     if params[:search_date] == nil
-      @chart = current_user.schedules.where(start_time: 2019-9-10)
+      @chart = current_user.schedules.where(start_time: 2019-9-10 07:00:00)
     else
       @chart = current_user.schedules.where.not(workhours: nil).where(start_time: params[:search_date].in_time_zone.all_month)
     end
-
-
 
   end
 
