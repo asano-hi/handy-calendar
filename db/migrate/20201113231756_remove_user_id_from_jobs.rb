15783590c48152f2ae76ce1db39c955ca6ca723e
asano <asanohiroto@asano-Mac.local>
2020-11-14 20:47:09 +0900

    schema.rbを修正

--- db/migrate/20201113231756_remove_user_id_from_jobs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromJobs < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :jobs, :user_id, :integer
-  end
-end
--- db/migrate/20201114111146_add_user_id_from_jobs.rb
+++
@@ -1,4 +0,0 @@
-class AddUserIdFromJobs < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
